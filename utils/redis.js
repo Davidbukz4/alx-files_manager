fec9d9464d402eac951f7c9aafaa313aaf5cf8ce
David Egwuatu <davidegwuatu2@gmail.com>
2023-09-02 05:18:19 +0000

    added task 0

---
+++ utils/redis.js
@@ -0,0 +1,37 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`ERROR: ${error}`);
+    });
+    this.connected = false;
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+  }
+
+  isAlive () {
+    return this.connected;
+  }
+
+  async get (key) {
+    const clientGet = promisify(this.client.get).bind(this.client);
+    const value = await clientGet(key);
+    return value;
+  }
+
+  async set (key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del (key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
