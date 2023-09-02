38c50c73567c300e52ad7a0772a3d89e723260f1
David Egwuatu <davidegwuatu2@gmail.com>
2023-09-02 10:53:46 +0000

    modified db(i)

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const URL = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
-  constructor () {
+  constructor() {
     MongoClient.connect(URL, (error, client) => {
       if (!error) {
         this.db = client.db(DATABASE);
@@ -16,18 +16,18 @@ class DBClient {
     });
   }
 
-  isAlive () {
+  isAlive() {
     if (this.db) {
       return true;
     }
     return false;
   }
 
-  async nbUsers () {
+  async nbUsers() {
     return this.db.collection('users').countDocuments();
   }
 
-  async nbFiles () {
+  async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
 }
